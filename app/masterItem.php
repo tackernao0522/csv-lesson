0e3c8197836822a002df722a4283f0d321f86874
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-03-08 22:05:09 +0900

    Modified app/masterItem.php

--- app/masterItem.php
+++ app/masterItem.php
@@ -9,5 +9,4 @@ class masterItem extends Model
     protected $fillable = [
         'combination_id', 'prin', 'chocolate', 'fresh_berries', 'raiins', 'pineapple', 'vanilla_ice_cream', 'brown_rice', 'roasted_soybeans', 'coconut', 'honey', 'miso', 'personal_flavor_print_file_name', 'personal_top_flavor_1', 'personal_top_flavor_2', 'personal_top_flavor_3', 'recommendation_1_id', 'recommendation_1_title', 'recommendation_1_compatibillity', 'recommendation_2_id', 'recommendation_2_title', 'recommendation_2_compatibility',
     ];
-    protected $primaryKey = null;
 }
